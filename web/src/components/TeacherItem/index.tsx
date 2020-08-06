9ef9e3b89fd7aa5bb3d43ef2a4ce203ce075f183
Andre Luiz <aluiz815@gmail.com>
2020-08-06 16:28:15 -0300

    Functionalities

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -2,31 +2,48 @@ import React from 'react';
 import './styles.css';
 
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
+import api from '../../services/api';
 
+export interface Teacher  {
+  id:number;
+  avatar:string;
+  bio:string;
+  cost:number;
+  subject:string;
+  name:string;
+  whatsapp:string;
+};
 
-const TeacherItem: React.FC = () => {
+interface TeacherItemProps {
+  teacher: Teacher
+}
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+
+  function createNewConnection(){
+    api.post('/connections',{
+      user_id:teacher.id
+    })
+  }
+  
   return (
     <article className="teacher-item">
     <header>
-      <img src="https://avatars1.githubusercontent.com/u/43641969?s=460&u=d854292859cd3419411d5b60b9a4f2ecf3fd6722&v=4" alt="Andre Luiz"/>
+      <img src={teacher.avatar} alt={teacher.name}/>
       <div>
-        <strong>Andre Luiz</strong>
-        <span>Matematica</span>
+      <strong>{teacher.name}</strong>
+      <span>{teacher.subject}</span>
       </div>
     </header>
-    <p>Entusiasta das melhores tecnologias de química avançada.
-      <br/><br/>
-      Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.
-    </p>
+      <p>{teacher.bio}</p>
     <footer>
       <p>
         Preço/hora
-        <strong>R$ 50,00</strong>
+        <strong>R$ {teacher.cost}</strong>
       </p>
-      <button type="button">
+      <a target="_blank" onClick={createNewConnection} href={`https://wa.me:/${teacher.whatsapp}`}>
           <img src={whatsappIcon} alt="Whatsapp"/>
           Entrar em contato
-        </button>
+        </a>
     </footer>
   </article>
   );
--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -1,18 +1,40 @@
-import React from 'react';
+import React, { useState, FormEvent } from 'react';
 import './styles.css';
 import PageHeader from '../../components/PageHeader';
-import TeacherItem from '../../components/TeacherItem';
+import TeacherItem,{ Teacher } from '../../components/TeacherItem';
 import Input from '../../components/Input';
 import Select from '../../components/Select';
+import api from '../../services/api';
+
+
 
 const TeacherList: React.FC = () => {
+
+  const [teachers,setTeachers]=useState([]);
+
+  const [subject,setSubject] = useState('');
+  const [week_day,setWeekDay] = useState('');
+  const [time,setTime] = useState('');
+
+  async function handleSearchTeachers(e:FormEvent) {
+    e.preventDefault();
+    const response = await api.get('/classes',{
+      params: {
+        subject,week_day,time
+      }
+    });
+    setTeachers(response.data);
+  }
+
   return (
     <div id="page-teacher-list" className="container">
       <PageHeader title="Estes são os proffys disponíveis.">
-        <form id="search-teachers">
+        <form id="search-teachers" onSubmit={handleSearchTeachers}>
         <Select 
           name="subject"
           label="Matéria"
+          value={subject}
+          onChange={e => setSubject(e.target.value)}
           options={[
             {value:'Artes',label:'Artes'},
             {value:'Biologia',label:'Biologia'},
@@ -21,7 +43,7 @@ const TeacherList: React.FC = () => {
             {value:'Fisica',label:'Fisica'},
             {value:'Geografia',label:'Geografia'},
             {value:'Historia',label:'Historia'},
-            {value:'Matematica',label:'Matematica'},
+            {value:'Matemática',label:'Matemática'},
             {value:'Quimica',label:'Quimica'},
             {value:'Português',label:'Português'},
           ]}
@@ -29,25 +51,28 @@ const TeacherList: React.FC = () => {
            <Select 
           name="week_day"
           label="Dia da semana"
+          value={week_day}
+          onChange={e => setWeekDay(e.target.value)}
           options={[
             {value:'0',label:'Domingo'},
             {value:'1',label:'Segunda-feira'},
             {value:'2',label:'Terça-feira'},
-            {value:'3 Fisica',label:'Quarta-feira'},
+            {value:'3',label:'Quarta-feira'},
             {value:'4',label:'Quinta-feira'},
             {value:'5',label:'Sexta-feira'},
             {value:'6',label:'Sabado'},
           ]}
           />  
-          <Input type="time" name="time" label="Hora"/>
+          <Input type="time" name="time" label="Hora"
+           value={time}
+           onChange={e => setTime(e.target.value)}/>
+           <button type="submit">Buscar</button>
         </form>
       </PageHeader>
       <main>
-       <TeacherItem/>
-       <TeacherItem/>
-       <TeacherItem/>
-       <TeacherItem/>
-       <TeacherItem/>
+        {teachers.map((teacher:Teacher) => {
+          return <TeacherItem key={teacher.id} teacher={teacher} />
+        })}
       </main>
     </div>
   );
